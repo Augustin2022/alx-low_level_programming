38e0a242f7295252486c0bbb97a833ff591f1d2c
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-02 06:57:32 -0700

    Task 3 v1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Description: Print alphabet in low case followd
+ * by a new line
+ * Return: return always 0
+ */
+int main(void)
+{
+	int x;
+	for(x = 97; x <= 122; x++)
+	{
+		putchar(x);
+		putchar("\n");
+	}
+	return (0):
+}
