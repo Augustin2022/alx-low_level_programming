66c67578ee808125e268eb470f53897264a96b9e
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-02 16:37:58 -0700

    Task 5 v1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: Print all sigle digit of base 10
+ * by a new line
+ * Return: return always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf(x);
+	}
+		putchar('\n');
+	return (0);
+}
