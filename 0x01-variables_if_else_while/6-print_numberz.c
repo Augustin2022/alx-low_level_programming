ca2480dafb66d6fd0c9b1ddb5c99dc1aa1a49b81
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-02 17:08:51 -0700

    Task 6 v1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: Print numbers of base 10 starting by 0 
+ * followd by a new line
+ * Return: return always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
