41bdd6147bbc9b85ec0e92b5d75197a7f55435ae
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-02 17:23:26 -0700

    Task 7 v1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Description: Print alphabet in lowcase in reverse followd
+ * by a new line
+ * Return: return always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 122; x <= 97; x--)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
