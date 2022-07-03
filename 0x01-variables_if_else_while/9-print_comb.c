2bede16b9a3b12818af9b529692f2a233584a195
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-02 18:38:38 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: Print numbers with space and comma followd
+ * by a new line
+ * Return: return always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		putchar(44);
+		putchar(20);
+	}
+		putchar('\n');
+	return (0);
+}
