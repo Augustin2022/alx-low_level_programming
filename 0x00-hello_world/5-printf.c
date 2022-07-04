86333be4069b2cba13dd643dadb2e9c2f0fb925d
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-03 18:04:00 -0700

    Task 5 v1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  program that print
+ *
+ * Description: a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
