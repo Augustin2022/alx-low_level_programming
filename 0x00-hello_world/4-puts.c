dce19190123575700b8da847662413d98dd9bed4
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-03 17:33:04 -0700

    Task 3-name v1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  program that print
+ *
+ * Description: program that prints exactly
+ * Programming is like building a multilingual puzzle,
+ * followed by a new line
+ * Return: always 0
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
