661e06e22f997ce12f8524436d5281903367a0a3
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-06 10:06:25 -0700

    Task 0 v1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print programme
+ *
+ * Description: a program that prints _putchar, followed by a new line.
+ * Return: Return always 0
+ */
+int main(void)
+{
+	printf("_putchr\n");
+	return (0);
+	
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+We learn here fonctions and loops
