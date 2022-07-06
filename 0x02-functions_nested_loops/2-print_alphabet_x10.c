ab2d28e5a7fa78f34644b8932a24030d2d59dd84
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-06 14:34:18 -0700

    Task2 v1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  a function that prints the alphabet, in lowercase
+ * Description:  a function that prints the alphabet, in lowercase,
+ *  followed by a new line.
+*/
+void printlphabet_x10(void)
+{
+
+int i, j;
+
+for (j = 1; j <= 10; j++)
+{
+
+	for (i = 97; i <= 122; i++)
+	{
+	_putchar(i);
+	}
+_putchar('\n');
+}
+}
