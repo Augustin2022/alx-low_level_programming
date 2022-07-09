9e62b583b7deec495ad19551b53e89bc1a630d93
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-09 11:36:29 -0700

    Task 7 better

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,11 @@ putchar ('\n');
 }
 else
 {
-for (i = 0 ; i <= n; i++)
+for (i = 0 ; i < n; i++)
 {
 putchar (92);
 putchar ('\n');
-for (j = 0 ; j <= i; j++)
+for (j = 0 ; j < i; j++)
 {
 putchar (' ');
 }
