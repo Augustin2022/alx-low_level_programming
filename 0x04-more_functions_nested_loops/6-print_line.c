f639aee44b42843e63e1c44979591d88fb59bf92
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-08 20:23:37 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - a function that prints 10 times the numbers,
+ * from 0 to 14, followed by a new liine
+ */ 
+
+void print_line(int n)
+{
+
+int i;
+if (n > 0)
+{
+	for (i = 0; i < 10 ; i++)
+	{
+	_putchar(95);
+	{
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
