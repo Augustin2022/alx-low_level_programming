31a164ab6161a8d688c2eef9d9ebe2091ca41d17
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-08 18:54:55 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 
 int i;
-for (i = 0; i <= 9; i++)
+for (i = 48; i <= 57; i++)
 {
-	if (i != 4 || != 9)
+	if ((i != 50) && (i != 52))
 	{
-	_putchar(i + '0');
+	_putchar(i);
 	}
 }
 	_putchar('\n');
