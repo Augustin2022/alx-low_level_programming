f5b740f690aaf3740a34c007c1ee66209e1ade82
Augustin2022 <jesmar.tech.pro@gmail.com>
2022-07-14 07:35:15 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp -> a function that  compares two strings.
+ * @s1: string parameter
+ * @s2: string parameter
+ * 
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+	n = strcmp(s1, s2);
+	return (n);
+
+}
